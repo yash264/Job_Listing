bbe48e30821dff763bfa134c6ea9fa6b7aee0688
Anshika6663999 <anshikabhadauriya69@gmail.com>
2026-03-15 00:54:46 +0530

    Update App.jsx

--- frontend /src/App.jsx
+++ frontend /src/App.jsx
@@ -1,28 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "./pages/Landing";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-import Dashboard from "./pages/Dashboard";
-import EmployerDashboard from "./pages/EmployerDashboard";
-import SeekerDashboard from "./pages/SeekerDashboard";
-import Jobs from "./pages/Jobs";
-import Candidates from "./pages/Candidates";
-import CandidateDetail from "./pages/CandidateDetail";
-import DashboardLayout from "./components/layout/DashboardLayout";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import DashboardLayout from './components/layout/DashboardLayout';
+import Dashboard from './pages/Dashboard';
+import Jobs from './pages/Jobs';
+import Candidates from './pages/Candidates';
+import CandidateDetail from './pages/CandidateDetail';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/dashboard/employer" element={<DashboardLayout><EmployerDashboard /></DashboardLayout>} />
-        <Route path="/dashboard/seeker" element={<DashboardLayout><SeekerDashboard /></DashboardLayout>} />
-        <Route path="/jobs" element={<DashboardLayout><Jobs /></DashboardLayout>} />
-        <Route path="/candidates" element={<DashboardLayout><Candidates /></DashboardLayout>} />
-        <Route path="/candidates/:id" element={<DashboardLayout><CandidateDetail /></DashboardLayout>} />
+        <Route element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="jobs" element={<Jobs />} />
+          <Route path="candidates" element={<Candidates />} />
+          <Route path="candidates/:id" element={<CandidateDetail />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   );
