e84d34097deddb67427ee4e81c207ff26133e386
Anshika6663999 <anshikabhadauriya69@gmail.com>
2026-03-23 22:04:51 +0530

    Update Register.jsx

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,118 +1,134 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link, useNavigate }  from "react-router-dom";
-import { Auth } from "../SvgImage/Auth";
-import { Footer } from "../components/Footer";
-import { ToastContainer,toast } from "react-toastify";
+import { Link, useNavigate } from "react-router-dom";
+import Auth from "../SvgImage/Auth";
+import Footer from "../components/Footer";
+import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.min.js";
 
 function Register() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const [name, setName] = useState([])
-    const [email, setEmail] = useState([])
-    const [password, setPassword] = useState([])
-    const navigate = useNavigate()
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    try {
+      const response = await axios.post(
+        "https://hiresathiserver.vercel.app/api/register",
+        { name, email, password }
+      );
 
-        try{
-            const response = await axios.post('https://hiresathiserver.vercel.app/api/register',
-                {  
-                    name:name,
-                    email:email,
-                    password:password
-                }
-            );
-            console.log(response.data);
-            if(response.data==="Email Already Exists"){
-                toast.error("Email Already Exists");
-            }
-            else if(response.data.message==="registered"){
-                toast.success("registered");
-            }
-        }
-        catch(error){
-            console.log(error);
-        }
+      if (response.data === "Email Already Exists") {
+        toast.error("Email Already Exists");
+      } else if (response.data.message === "registered") {
+        toast.success("Registered successfully");
+        navigate("/User/login");
+      }
+    } catch (error) {
+      console.error(error);
+      toast.error("Registration failed");
     }
+  };
 
-    useEffect(() => {
-        
-        const verifyUser = async () => {
-            const isTokenValid = await checkToken();
-            if (isTokenValid.isValid) {
-                navigate("/User/Dashboard"); // Redirect to main page if token is valid
-            }
-        };
-
-        verifyUser();
-
-    }, []);
-
-    const checkToken = async () => {
-        const token = localStorage.getItem('authToken');
-        console.log(token);
-
-        if (!token) return false;
-
-        try {
-
-            const response = await axios.post('https://hiresathiserver.vercel.app/api/verify-token', {}, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            console.log(response.data);
-            return { isValid: response.data.valid, data: response.data.data };
-        } catch (error) {
-            console.error(error);
-            return false;
-        }
+  useEffect(() => {
+    const verifyUser = async () => {
+      const isTokenValid = await checkToken();
+      if (isTokenValid.isValid) {
+        navigate("/User/dashBoard");
+      }
     };
+    verifyUser();
+  }, [navigate]);
 
-    return (
-        <>
-            <div class="container px-4 text-center">
-                <div class="row gx-5">
-                    <div class="col">
-                        <div class="p-3">
-                            <Auth />
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="p-3">
-                            <h4>Register</h4>
-                            <form class="row g-3" onSubmit={handleSubmit} >
-                                <div class="col-md-8">
-                                    <label for="name" class="form-label">Name</label>
-                                    <input type="text" class="form-control"  onChange={(e) => setName(e.target.value)} />
-                                </div>
-                                <div class="col-md-8">
-                                    <label for="email" class="form-label">Email</label>
-                                    <input type="email" class="form-control"  onChange={(e) => setEmail(e.target.value)} />
-                                </div>
-                                <div class="col-md-8">
-                                    <label for="password" class="form-label">Password</label>
-                                    <input type="password" class="form-control" onChange={(e) => setPassword(e.target.value)} />
-                                </div>
+  const checkToken = async () => {
+    const token = localStorage.getItem("authToken");
+    if (!token) return false;
 
-                                <div class="col-12">
-                                    <button type="submit" class="btn btn-primary">Register</button>
-                                    <button class="btn btn-outline-secondary" ><Link class="nav-link" to="../User/login">Already Registered</Link></button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <Footer />
-            <ToastContainer />
-        </>
-    )
+    try {
+      const response = await axios.post(
+        "https://hiresathiserver.vercel.app/api/verify-token",
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      return { isValid: response.data.valid, data: response.data.data };
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  };
+
+  return (
+    <>
+      <div className="container px-4 text-center main-bg py-5">
+        <div className="row gx-5 align-items-center">
+          {/* Left Column with SVG */}
+          <div className="col-md-6">
+            <Auth />
+          </div>
+
+          {/* Right Column with Form */}
+          <div className="col-md-6">
+            <h4 className="link-custom mb-4">Register</h4>
+            <form className="row g-3" onSubmit={handleSubmit}>
+              <div className="col-md-10 mx-auto">
+                <label htmlFor="name" className="form-label">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  className="form-control"
+                  onChange={(e) => setName(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className="col-md-10 mx-auto">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  className="form-control"
+                  onChange={(e) => setEmail(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className="col-md-10 mx-auto">
+                <label htmlFor="password" className="form-label">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  className="form-control"
+                  onChange={(e) => setPassword(e.target.value)}
+                  required
+                />
+              </div>
+
+              <div className="col-12 mt-3">
+                <button type="submit" className="btn btn-custom me-2">
+                  Register
+                </button>
+                <Link to="../User/login" className="btn btn-outline-secondary">
+                  Already Registered?
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <ToastContainer />
+      <Footer />
+    </>
+  );
 }
 
-export default Register;
+export default Register;       
