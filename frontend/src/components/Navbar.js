3485a6b641775e5d35db5cb162983e01ceaf028f
Anshika6663999 <anshikabhadauriya69@gmail.com>
2026-03-23 21:45:01 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,42 +1,50 @@
 import React from "react";
-import { useNavigate, Link }  from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 export const Navbar = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("authToken");
+    navigate("/");
+  };
 
-    const logout = (e) => {
-        e.preventDefault();
-        localStorage.removeItem("authToken");
-        navigate("/");
-    }
-
-    return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Services</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <Link class="nav-link" to="../User/dashBoard">DashBoard</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="../User/notification">Notification</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="../User/pastApplication">Past Application</Link>
-                        </li>
-                    </ul>
-                    <div>
-                        <button type="button" class="btn btn-outline-danger" onClick={logout} >Logout</button>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg custom-navbar">
+      <div className="container-fluid">
+        <a className="navbar-brand link-custom" href="#">Services</a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link link-custom" to="../User/dashBoard">DashBoard</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link link-custom" to="../User/notification">Notification</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link link-custom" to="../User/pastApplication">Past Application</Link>
+            </li>
+          </ul>
+          <button
+            type="button"
+            className="btn btn-custom"
+            onClick={logout}
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
 };
