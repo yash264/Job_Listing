8ac47d3f8d04751c9520d2093a3510532f8c774b
Anshika6663999 <anshikabhadauriya69@gmail.com>
2026-03-24 00:14:45 +0530

    Update Nav.js

--- frontend/src/pages/Admin/Nav.js
+++ frontend/src/pages/Admin/Nav.js
@@ -1,39 +1,60 @@
 import React from "react";
-import { Link, useNavigate }  from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
 export const Nav = () => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("authToken");
+    navigate("/");
+  };
 
-    const logout = (e) => {
-        e.preventDefault();
-        localStorage.removeItem("authToken");
-        navigate("/");
-    }
+  return (
+    <nav className="navbar navbar-expand-lg header">
+      <div className="container-fluid">
+        <a className="navbar-brand brand" href="#">
+          Services
+        </a>
 
-    return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">Services</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <Link class="nav-link" to="../admin/adminDash">DashBoard</Link>
-                        </li>
-                        <li class="nav-item">
-                            <Link class="nav-link" to="../admin/createJob">Create a Job</Link>
-                        </li>
-                    </ul>
-                    <div>
-                        <button type="button" class="btn btn-outline-danger" onClick={logout} >Logout</button>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    )
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link nav-link-custom" to="../admin/adminDash">
+                Dashboard
+              </Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link nav-link-custom" to="../admin/createJob">
+                Create a Job
+              </Link>
+            </li>
+          </ul>
+
+          <div>
+            <button
+              type="button"
+              className="btn btn-custom"
+              onClick={logout}
+            >
+              Logout
+            </button>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
+  
