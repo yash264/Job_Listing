cbbefac265434af049d361c7260372ff828cab42
Anshika6663999 <anshikabhadauriya69@gmail.com>
2026-03-24 09:35:14 +0530

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,34 +1,50 @@
-  import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+
   return (
     <header className="custom-navbar p-3">
       <div className="container-fluid d-flex justify-content-between align-items-center">
-        {/* Logo / Brand */}
-        <h3 className="link-custom m-0">HireSathi</h3>
+        <NavLink to="/" className="navbar-brand link-custom m-0">
+          HireSathi
+        </NavLink>
 
-        {/* Navigation Links */}
-        <nav>
-          <ul className="d-flex list-unstyled m-0">
-            <li className="mx-3">
-              <Link className="nav-link link-custom" to="/">Home</Link>
+        <button
+          className="navbar-toggler border-0 bg-transparent"
+          type="button"
+          aria-label="Toggle navigation"
+          onClick={() => setOpen((s) => !s)}
+        >
+          {open ? <FaTimes className="link-custom" /> : <FaBars className="link-custom" />}
+        </button>
+
+        <nav className={`collapse navbar-collapse ${open ? "show" : ""}`}>
+          <ul className="navbar-nav ms-auto d-flex align-items-center">
+            <li className="nav-item mx-2">
+              <NavLink to="/" className="nav-link link-custom" end>
+                Home
+              </NavLink>
             </li>
-            <li className="mx-3">
-              <Link className="nav-link link-custom" to="/features">Features</Link>
+            <li className="nav-item mx-2">
+              <NavLink to="/features" className="nav-link link-custom">
+                Features
+              </NavLink>
             </li>
-            <li className="mx-3">
-              <Link className="nav-link link-custom" to="/about">About</Link>
+            <li className="nav-item mx-2">
+              <NavLink to="/about" className="nav-link link-custom">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item mx-2">
+              <NavLink to="/User/login" className="nav-link link-custom">
+                Login
+              </NavLink>
             </li>
           </ul>
         </nav>
-
-        {/* Optional Icons  */}
-        <div className="d-flex align-items-center">
-          <FaBars className="link-custom mx-2" />
-          <FaTimes className="link-custom mx-2" />
-        </div>
       </div>
     </header>
   );
