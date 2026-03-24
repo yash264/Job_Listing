808df6a83537df0a9c3a2365a3797aabbc3dd1bf
Anshika6663999 <anshikabhadauriya69@gmail.com>
2026-03-24 15:42:06 +0530

    Index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css"; // load bootstrap first
+import "./index.css"; // then load your theme so it can override bootstrap
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
@@ -13,3 +13,12 @@ root.render(
 );
 
 reportWebVitals();
+
+// Unregister any service workers to avoid stale cached bundles
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(registrations => {
+    registrations.forEach(reg => {
+      reg.unregister().catch(() => {});
+    });
+  }).catch(() => {});
+}
